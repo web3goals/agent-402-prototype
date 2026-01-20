cbdd6e9c6976fea66277fc6535b1a0dd7f24d869
kiv1n <aloha@kiv1n.ru>
2026-01-20 12:35:03 +0300

    Add trade execution endpoint using VVS swap SDK

---
+++ app/app/api/trades/route.ts
@@ -0,0 +1,59 @@
+import { createFailedApiResponse, createSuccessApiResponse } from "@/lib/api";
+import axios from "axios";
+import {
+  fetchBestTrade,
+  PoolType,
+  BuiltInChainId,
+  utils as SwapSdkUtils,
+} from "@vvs-finance/swap-sdk";
+
+export async function POST() {
+  try {
+    console.log("[API] Executing trade...");
+
+    const chainId = BuiltInChainId.CRONOS_MAINNET;
+    const inputToken = "NATIVE"; // VVS
+    const outputToken = "0x2D03bECE6747ADC00E1a131BBA1469C15fD11e03"; // VVS
+    const amount = "1.0";
+    const clientId = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"; // or set process.env.SWAP_SDK_QUOTE_API_CLIENT_ID_338 in nodejs
+
+    console.log("Here");
+
+    const trade = await fetchBestTrade(
+      chainId,
+      inputToken,
+      outputToken,
+      amount,
+      {
+        poolTypes: [
+          PoolType.V2,
+          PoolType.V3_100,
+          PoolType.V3_500,
+          PoolType.V3_3000,
+          PoolType.V3_10000,
+        ],
+        maxHops: 3,
+        maxSplits: 2,
+        quoteApiClientId: clientId,
+      },
+    );
+
+    console.log(`trade: ${SwapSdkUtils.formatTrade(trade)}`);
+    console.log(trade);
+
+    return createSuccessApiResponse({ message: "Ok!" });
+  } catch (error) {
+    console.error(
+      "[API] Failed to execute trade, error:",
+      axios.isAxiosError(error)
+        ? error.response?.data || error.message
+        : error instanceof Error
+        ? error.message
+        : String(error),
+    );
+    return createFailedApiResponse(
+      { message: "Internal server error, try again later" },
+      500,
+    );
+  }
+}
